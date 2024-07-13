31f9ca58bfac13940e4b66d1af6a8b0a75293cd6
LukeParkerDev <10430890+Hona@users.noreply.github.com>
2024-07-13 11:23:06 +1000

    empty global usings

--- template/src/VerticalSliceArchitectureTemplate/GlobalUsings.cs
+++ template/src/VerticalSliceArchitectureTemplate/GlobalUsings.cs
@@ -1,6 +1,1 @@
-﻿global using Microsoft.EntityFrameworkCore;
-global using VerticalSliceArchitectureTemplate.Common.Exceptions;
-global using VerticalSliceArchitectureTemplate.Common.Features;
-global using VerticalSliceArchitectureTemplate.Common.Persistence;
-global using Immediate.Handlers.Shared;
-global using Microsoft.AspNetCore.Mvc;
+﻿
